27d7bb85e10e526b72afa6ee25377cec006af472
LeHoangTuan69 <tuanlhgcs200305@fpt.edu.vn>
2023-12-09 11:06:28 +0700

    Update video by using ScreenPal create video URL

--- src/components/VideoImg/ProfileVideos.js
+++ src/components/VideoImg/ProfileVideos.js
@@ -1,9 +1,5 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-// import profile1 from "../../Assets/ProfileVideos/video1.mp4";
-// import profile2 from "../../Assets/ProfileVideos/video2.mp4";
-// import profile3 from "../../Assets/ProfileVideos/video3.mp4";
-// import profile4 from "../../Assets/ProfileVideos/video4.mp4";
 
 function ProfileVideo() {
   return (
@@ -17,23 +13,81 @@ function ProfileVideo() {
         {/* <Col xs={1} md={1} className="tech-icons">
           <video src={profile1} controls className="video-stand"></video>
           <p>Hello</p>
-        </Col>
-        <Col xs={1} md={1} className="tech-icons">
-          <video src={profile2} controls className="video-stand"></video>
-          <p>Hello</p>
-        </Col>
-        <Col xs={1} md={1} className="tech-icons">
-          <video src={profile3} className="video-stand" controls></video>
-          <p>Hello</p>
         </Col> */}
+
+        <Col xs={1} md={1} className="tech-icons">
+          <iframe
+            width="576"
+            height="1024"
+            className="video-stand"
+            title="TrongHoang-Tiktok1"
+            src="https://screenpal.com/player/c0lQeQVm5HA?width=576&height=1024&ff=1&bg=transparent&download=1&embed=1"
+            allowfullscreen="true"
+          ></iframe>
+          <p>Hello</p>
+        </Col>
+
+        <Col xs={1} md={1} className="tech-icons">
+          <iframe
+            width="720"
+            height="1280"
+            title="TrongHoang-VLU2"
+            className="video-stand"
+            src="https://screenpal.com/player/c0lQejVm5Hz?width=720&height=1280&ff=1&bg=transparent&download=1&embed=1"
+            allowfullscreen="true"
+          ></iframe>
+          <p>Hello</p>
+        </Col>
+
+        <Col xs={1} md={1} className="tech-icons">
+          <iframe
+            width="720"
+            height="1280"
+            title="TrongHoang-VLU3"
+            className="video-stand"
+            src="https://screenpal.com/player/c0lQejVm5Ha?width=720&height=1280&ff=1&bg=transparent&download=1&embed=1"
+            allowfullscreen="true"
+          ></iframe>
+          <p>Hello</p>
+        </Col>
+
+        <Col xs={1} md={1} className="tech-icons">
+          <iframe
+            width="720"
+            height="1280"
+            title="TrongHoang-Tiktok2"
+            className="video-stand"
+            src="https://screenpal.com/player/c0lQe7Vm5Ku?width=720&height=1280&ff=1&bg=transparent&download=1&embed=1"
+            allowfullscreen="true"
+          ></iframe>
+        </Col>
       </Row>
 
-      {/* <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
         <Col xs={1} md={1} className="tech-icons">
-          <video src={profile4} className="video-lying" controls></video>
-          Hello
+          <iframe
+            width="640"
+            height="360"
+            className="video-lying"
+            title="TrongHoang-VLU1"
+            src="https://screenpal.com/player/c0lQeQVm5Hk?width=640&height=360&ff=1&bg=transparent&download=1&embed=1"
+            allowfullscreen="true"
+          ></iframe>
+          <p>Hello</p>
         </Col>
-      </Row> */}
+
+        <Col xs={1} md={1} className="tech-icons">
+          <iframe
+            width="600"
+            height="360"
+            className="video-lying"
+            title="TrongHoang-VLU4"
+            src="https://screenpal.com/player/c0lQe7Vm5Kv?width=600&height=360&ff=1&bg=transparent&download=1&embed=1"
+            allowfullscreen="true"
+          ></iframe>
+          <p>Hello</p>
+        </Col>
+      </Row>
     </div>
   );
 }
